a82bc17b56cb922e02d5011ff351ba45de1594d0
ProjectRabbit <1512849856@qq.com>
2025-07-06 03:28:59 +0800

    feat(Init): 条件选择

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,26 @@
 import './App.css'
 import MyButton from "./components/my-button/MyButton.tsx";
 import User from "./components/user/User.tsx";
+import MyUser from "./components/user/MyUser.tsx";
 
 function App() {
 
-  return (
-    <div>
-        <h1>欢迎来到我的应用</h1>
-        <MyButton/>
-        <br/>
-        <User/>
-    </div>
-  )
+    const isUser = true
+    let content
+    if (isUser){
+        content = <MyUser/>
+    }else {
+        content = <User/>
+    }
+
+    return (
+        <div>
+            <h1>欢迎来到我的应用</h1>
+            <MyButton/>
+            <br/>
+            {content}
+        </div>
+    )
 }
 
 export default App
