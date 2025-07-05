cc253bae672c76a2ffd1a91b75352a65db2054bb
ProjectRabbit <1512849856@qq.com>
2025-07-06 03:51:37 +0800

    feat(Init): 构建井字棋项目

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,10 @@
 import './App.css'
-import MyButton from "./components/my-button/MyButton.tsx";
-import {useState} from "react";
 
 function App() {
 
-
-    const [count,setCount] = useState(0)
-
     return (
-        <div>
-            <h1>共同更新的计数器</h1>
-            <MyButton count={count} setCount={()=> setCount(count+1)}/>
-            <br/>
-            <MyButton count={count} setCount={()=> setCount(count+1)}/>
+        <div className="App">
+
         </div>
     )
 }
